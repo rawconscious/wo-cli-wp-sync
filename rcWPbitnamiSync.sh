469466e2f50c4b8561dd0a28e90b6b6e08a75f27
Manoj <manoj@rawconscious.com>
2022-09-08 14:39:30 +0530

    bitnami sync script version 1

--- rcWPbitnamiSync.sh
+++ rcWPbitnamiSync.sh
@@ -9,8 +9,50 @@
 # ---------------------
 # scp lolly:~/bitnami_wordpress-2022-09-01-f457d3d.sql /var/www/lolly/
 # cd /var/www/lolly && wp db import bitnami_wordpress-2022-09-01-f457d3d.sql --allow-root
-# wp search-replace 'https://www.lolly.com' 'http://localhost:8105' --skip-columns=guid --allow-root
-# wp search-replace 'http://www.lolly.com' 'http://localhost:8105' --skip-columns=guid --allow-root
-# wp search-replace 'https://lolly.com' 'http://localhost:8105' --skip-columns=guid --allow-root
-# wp search-replace 'http://lolly.com' 'http://localhost:8105' --skip-columns=guid --allow-root
+# wp search-replace "https://www.$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+# wp search-replace "http://www.$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+# wp search-replace "https://$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+# wp search-replace "http://$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
 # rsync -a lolly:/bitnami/wordpress/wp-content/uploads/ /var/www/lolly/wp-content/uploads/
+
+echo "enter the project name:"
+read project_name
+
+rc_uuid=$(date '+%Y-%m-%d-%H-%M-%S')-$( cat /proc/sys/kernel/random/uuid )
+
+sudo ssh lolly 'bash -s' << EOF
+
+cd /bitnami/wordpress
+sudo wp db export $rc_uuid.sql
+
+EOF
+
+sudo scp $project_name:/bitnami/wordpress/$rc_uuid.sql /var/www/$project_name/
+
+sudo ssh lolly 'bash -s' << EOF
+
+cd /bitnami/wordpress
+sudo rm $rc_uuid.sql
+
+EOF
+
+cd /var/www/$project_name/
+
+echo "enter the project root domain ( Example example.com):"
+read project_root_domain
+echo "enter the project local domain with port ( Example localhost:8080):"
+read project_local_domain
+
+wp db import $rc_uuid.sql --allow-root
+
+wp search-replace "https://www.$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+wp search-replace "http://www.$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+wp search-replace "https://$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+wp search-replace "http://$project_root_domain" "http://$project_local_domain" --skip-columns=guid --allow-root
+
+sudo rsync -a lolly:/bitnami/wordpress/wp-content/uploads/ /var/www/lolly/wp-content/uploads/
+sudo chown -R www-data:www-data wp-content
+
+
+
+
